cf6e4df0027cacfd3e35b496da9f9dc01b58de89
jonoman55 <jonoman55@yahoo.com>
2022-07-04 00:58:46 -0400

    types & initial state added to store

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,13 +1,24 @@
+// DOCS : https://github.com/pmndrs/zustand
 import create from 'zustand';
 import { persist } from 'zustand/middleware';
 import axios from 'axios';
 
 import { BASE_URL } from '../utils';
 
+import { IUser } from '../types';
+
+interface IAuthState {
+    userProfile: IUser | null;
+    allUsers: IUser[];
+    addUser: (user: IUser) => void;
+    removeUser: () => void;
+    fetchAllUsers: () => Promise<void>; 
+};
+
 const authStore = (set: any) => ({
     userProfile: null,
     allUsers: [],
-    addUser: (user: any) => set({ userProfile: user }),
+    addUser: (user: IUser) => set({ userProfile: user }),
     removeUser: () => set({ userProfile: null }),
     fetchAllUsers: async () => {
         const { data } = await axios.get(`${BASE_URL}/api/users`);
@@ -15,10 +26,8 @@ const authStore = (set: any) => ({
     },
 });
 
-const useAuthStore = create((
-    persist(authStore, {
-        name: 'auth',
-    })
-));
+const useAuthStore = create((persist<IAuthState>(authStore, {
+    name: 'auth'
+})));
 
 export default useAuthStore;
